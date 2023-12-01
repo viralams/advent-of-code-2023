d555fe6373f44544f455c85e2fcd79260cd6b9df
Shiva Sharan Viralam Sreedar <105316316+viralams@users.noreply.github.com>
2023-12-01 07:40:38 -0600

    Parts confused

--- day01/src/bin/part1.rs
+++ day01/src/bin/part1.rs
@@ -1,53 +1,27 @@
-use std::collections::HashMap;
-
 const TEST_DATA: &str = include_str!("input1.txt");
 
-
 fn main() {
-    println!("{}", TEST_DATA);
     let mut total = 0;
     let mut lines = TEST_DATA.lines();
+
     for line in lines {
-        let line_val = process_line(&str::to_string(line));
-        println!("Line val: {:?}", line_val);
-        total = total + line_val;
+        let chars = line.chars();
+        let mut first_num: Option<char> = None;
+        let mut last_num: Option<char> = None;
+        let mut line_vec = vec![];
+
+        for char in chars {
+            if char.is_numeric() {
+                if first_num == None {
+                    first_num = Some(char);
+                }
+                last_num = Some(char);
+            }
+        }
+        line_vec.push(first_num.unwrap());
+        line_vec.push(last_num.unwrap());
+        let line_val = line_vec.into_iter().collect::<String>().parse::<i32>().unwrap();
+        total = total+ line_val;
     }
     println!("Total is : {:?}", total);
-}
-
-fn process_line(line: &str) -> u32 {
-    let mut it = (0..line.len()).filter_map(|index| {
-        let reduced_line = &line[index..];
-        let result = if reduced_line.starts_with("one") {
-            '1'
-        } else if reduced_line.starts_with("two") {
-            '2'
-        } else if reduced_line.starts_with("three") {
-            '3'
-        } else if reduced_line.starts_with("four") {
-            '4'
-        } else if reduced_line.starts_with("five") {
-            '5'
-        } else if reduced_line.starts_with("six") {
-            '6'
-        } else if reduced_line.starts_with("seven") {
-            '7'
-        } else if reduced_line.starts_with("eight") {
-            '8'
-        } else if reduced_line.starts_with("nine") {
-            '9'
-        } else {
-            reduced_line.chars().next().unwrap()
-        };
-
-        result.to_digit(10)
-    });
-    let first = it.next().expect("first should be a number");
-
-    match it.last() {
-        Some(num) => format!("{first}{num}"),
-        None => format!("{first}{first}"),
-    }
-        .parse::<u32>()
-        .expect("rendered value should be a valid number in the valid range")
 }
